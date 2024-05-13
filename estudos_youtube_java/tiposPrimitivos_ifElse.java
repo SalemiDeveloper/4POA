53584a71a8aeddbc3a90bf65c0d1e0518224d31f
Pedro Salemi <128959821+SalemiDeveloper@users.noreply.github.com>
2024-05-13 14:20:17 -0300

    Create tiposPrimitivos_ifElse.java

---
+++ estudos_youtube_java/tiposPrimitivos_ifElse.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+        int minhaIdade = 22;
+        String nome = "Pedro";
+        double idade = 22.50;
+        float salario = 400.20F;
+
+        if(idade > 22.60)
+        {
+            System.out.println("maior");
+        } else if (idade == 22.60) {
+            System.out.println("igual");
+        } else {
+            System.out.println("menor")
+        }
+    }
+}
