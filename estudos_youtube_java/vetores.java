627e1ec362b52eb19ab06ab81cc2cc843120c5e9
Pedro Salemi <128959821+SalemiDeveloper@users.noreply.github.com>
2024-05-13 14:20:35 -0300

    Create vetores.java

---
+++ estudos_youtube_java/vetores.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+        //já inserindo os valores no vetor
+        int[] idades = {2, 3, 4};
+        idades[0] = 4;
+        boolean[] saoBrasil = {true, false, true, false};
+
+        //ou inicializando ele vazio porém ja dizendo o tamanho para depois preenchê-lo
+        int idade[] = new int[10];
+        String[] nomes = new String[10];
+        boolean[] saoBrasileiros = new boolean[10];
+
+        //para saber/retornar o tamanho do vetor
+        idades.length;
+        idade.length;
+
+    }
+}
