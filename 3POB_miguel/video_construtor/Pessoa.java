6fc2bdb17f46975d40a57db8e1406a3c12578440
Pedro Salemi <128959821+SalemiDeveloper@users.noreply.github.com>
2024-05-14 14:11:11 -0300

    Create Pessoa.java

---
+++ 3POB_miguel/video_construtor/Pessoa.java
@@ -0,0 +1,14 @@
+package exemplo;
+
+public class Pessoa {
+
+	public String nome;
+	public int idade;
+	
+	
+	//CONSTRUTOR
+	public Pessoa(String nome, int idade) {
+		this.nome = nome;
+		this.idade = idade;
+	}
+}
