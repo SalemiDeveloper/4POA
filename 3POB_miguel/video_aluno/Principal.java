706930e1ebaabbf19986c077ed9bc9522ff9b9fc
Pedro Salemi <128959821+SalemiDeveloper@users.noreply.github.com>
2024-05-15 13:20:16 -0300

    Create Principal.java

---
+++ 3POB_miguel/video_aluno/Principal.java
@@ -0,0 +1,12 @@
+package exemplo;
+
+import java.util.Scanner;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		Aluno a1 = new Aluno ("Joao", "123");
+		System.out.println(a1);
+	}
+
+}
