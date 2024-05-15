4834ca499ccbf595fcefa9f914d8e7938137883d
Pedro Salemi <128959821+SalemiDeveloper@users.noreply.github.com>
2024-05-15 13:24:17 -0300

    Create video_professor.java

---
+++ 3POB_miguel/video_professor.java
@@ -0,0 +1,20 @@
+package exemplo;
+
+public class Professor {
+
+	public String nome;
+	public String mat;
+	
+	public Professor(String nome, String mat) {
+		this.nome = nome;
+		this.mat = mat;
+	}
+	
+	public void lancarNotas() {
+		
+	}
+	
+	public void lancarFaltas() {
+		
+	}
+}
