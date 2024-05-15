94d0a2898154e6ad144458977adfe2675097bd03
Pedro Salemi <128959821+SalemiDeveloper@users.noreply.github.com>
2024-05-15 13:43:32 -0300

    Create Aviao.java

---
+++ 3POB_miguel/video_interface/Aviao.java
@@ -0,0 +1,25 @@
+package exemplo;
+
+public class Aviao implements Voador{
+
+	@Override
+	public void voar() {
+		//exemplo
+		System.out.println("Aviao voando");
+		
+	}
+
+	@Override
+	public void pousar() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void decolar() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+
+}
