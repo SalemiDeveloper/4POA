9f7896a7740360e56c2e4e940f47662864d5778f
Pedro Salemi <128959821+SalemiDeveloper@users.noreply.github.com>
2024-05-15 13:43:14 -0300

    Create Voador.java

---
+++ 3POB_miguel/video_interface/Voador.java
@@ -0,0 +1,11 @@
+package exemplo;
+
+//Interface é um contrato que as classes assinam para implementar
+//Isso permite a padronização
+public interface Voador {
+
+	public void voar();
+	public void pousar();
+	public void decolar();
+	
+}
