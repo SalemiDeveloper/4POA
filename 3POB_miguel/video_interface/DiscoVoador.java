56898a811c85ad3c0adc1562abbe82272dcafd23
Pedro Salemi <128959821+SalemiDeveloper@users.noreply.github.com>
2024-05-15 13:43:53 -0300

    Create DiscoVoador.java

---
+++ 3POB_miguel/video_interface/DiscoVoador.java
@@ -0,0 +1,23 @@
+package exemplo;
+
+public class DiscoVoador implements Voador{
+
+	@Override
+	public void voar() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void pousar() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void decolar() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
